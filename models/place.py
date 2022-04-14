8b5aec52e47724f7595a5b48a921c93f25e62e6c
cristhian1107 <cristhian.cjaa@gmail.com>
2022-04-13 21:22:20 -0500

    Feat! (DBStorage) Amenity... and BOOM!

--- models/place.py
+++ models/place.py
@@ -5,6 +5,21 @@ from sqlalchemy import Column, String, Float, Integer, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from os import getenv
 
+place_amenity = Table(
+    'place_amenity',
+    Base.metadata,
+    Column(
+            'place_id',
+            String(60),
+            ForeignKey('places.id'),
+            nullable=False),
+    Column(
+            'amenity_id',
+            String(60),
+            ForeignKey('amenities.id'),
+            nullable=False)
+)
+
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -20,12 +35,18 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
+    amenity_ids = []
 
     if getenv('HBNB_TYPE_STORAGE') == "db":
         reviews = relationship(
                                 'Review',
                                 cascade='all, delete',
                                 backref='place')
+        amenities = relationship(
+                                'Amenity',
+                                secondary=place_amenity,
+                                viewonly=False,
+                                back_populates="place_amenities")
     else:
         @property
         def reviews(self):
@@ -38,3 +59,15 @@ class Place(BaseModel, Base):
                 if review.place_id == self.id:
                     list_reviews.append(review)
             return list_reviews
+
+        @property
+        def amenities(self):
+            """Getter attribute amenities that returns the list of Amenity"""
+            from models.amenity import Amenity
+            from models.__init__ import storage
+            all_amenities = storage.all(Amenity)
+            list_return = []
+            for key, value in all_amenities.items():
+                if self.id == value.place_id:
+                    list_return.append(value)
+            return list_return
